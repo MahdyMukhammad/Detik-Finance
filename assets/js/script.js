2393c57c66c1a1785e365724c3559c0d4faeab2f
Mukhammad Mahdy <mahdy.mukhammad@gmail.com>
2023-08-16 15:38:57 +0700

    commit first

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+var swiper = new Swiper(".reviews-slider", {
+  spaceBetween: 20,
+  grabCursor: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    540: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
